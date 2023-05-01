b04ac4c357327bbba67eea80640fbb3366db4a49
Xander Adams <xanderadams555@gmail.com>
2023-05-01 12:48:38 -0500

    progress?

--- lib/journal_screen.dart
+++ lib/journal_screen.dart
@@ -10,10 +10,26 @@ class JournalPage extends StatefulWidget {
 
 class _JournalPageState extends State<JournalPage> {
   List<Entry> entries = [];
+  @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: const Text('Journal Page'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(() {
+          entries.add(Entry(title, contents, color, tags));
+        }),
+        backgroundColor: Colors.blueAccent,
+        child: const Icon(Icons.add)
       ),
     );
   }
