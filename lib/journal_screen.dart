4db0dd0555ada073b48c56e7fd249ad09a996f6e
hailey <hailey.morganh@gmail.com>
2023-05-01 13:00:42 -0500

    hamburger
    (drawer)

--- lib/journal_screen.dart
+++ lib/journal_screen.dart
@@ -16,6 +16,34 @@ class _JournalPageState extends State<JournalPage> {
       appBar: AppBar(
         title: const Text('Journal Page'),
       ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+
+
       body: Center(
         child: SingleChildScrollView(
           child: Column(
