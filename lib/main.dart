fcb0bb841bf52366ac501564228769c183c9f1a8
hailey <hailey.morganh@gmail.com>
2023-05-02 13:49:53 -0500

    eric help!
    put everything in the same file, add entry screen navigation

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,35 @@
 import 'package:flutter/material.dart';
 
-List<Entry> entries = [];
+
 
 class Entry {
   String title;
   String contents;
   Color color;
-  List<Tag> tags;
+  String tags;
 
   Entry(this.title, this.contents, this.color, this.tags);
+}
+
+List<Entry> entries = [];
+
+void main() {
+  entries.add(Entry("here's my title", "heres my theoretically longer string", Colors.blueAccent, 'tag,tag,tag,cool'));
+  entries.add(Entry("here's my other title", "heres my kind of longer string", Colors.yellowAccent, 'tag,tag,cool'));
+  entries.add(Entry("here's my third title", "heres my theoretically super duper extra long string", Colors.redAccent, 'tag,tag,tag,cool'));
+
+  print("Entry: " + entries.length.toString());
+
+  runApp(
+   MaterialApp(
+     title: '',
+     initialRoute: '/',
+     routes: {
+       '/': (context) => const JournalPage(),
+       '/new_entry': (context) => const NewEntry(),
+     },
+   )
+ );
 }
 
 class JournalPage extends StatefulWidget {
@@ -17,6 +38,8 @@ class JournalPage extends StatefulWidget {
   @override
   State<JournalPage> createState() => _JournalPageState();
 }
+
+
 
 class _JournalPageState extends State<JournalPage> {
   List<Entry> entries = [];
@@ -29,6 +52,46 @@ class _JournalPageState extends State<JournalPage> {
       body: Center(
         child: SingleChildScrollView(
           child: Column(
+            children: [
+              for(int i = 0; i < entries.length; i++)
+                Text("HI" + i.toString())
+              // siezebox default size
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/new_entry');
+            //entries.add(Entry(title, contents, color, tags));
+          },
+          backgroundColor: Colors.blueAccent,
+          child: const Icon(Icons.add)
+      ),
+    );
+  }
+}
+
+class NewEntry extends StatefulWidget {
+  const NewEntry({super.key});
+
+  @override
+  State<NewEntry> createState() => _NewEntryState();
+}
+
+
+
+class _NewEntryState extends State<NewEntry> {
+  List<Entry> entries = [];
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('E Page'),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
 
           ),
@@ -36,7 +99,7 @@ class _JournalPageState extends State<JournalPage> {
       ),
       floatingActionButton: FloatingActionButton(
           onPressed: () => setState(() {
-            entries.add(Entry(title, contents, color, tags));
+            //entries.add(Entry(title, contents, color, tags));
           }),
           backgroundColor: Colors.blueAccent,
           child: const Icon(Icons.add)
